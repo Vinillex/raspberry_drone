9f6a08123ec07a07fba80580a91c5ac401184de6
Vinillex <shettigarvinit23299@gmail.com>
2024-02-17 20:22:28 +0530

    calibration

--- mpu.py
+++ mpu.py
@@ -3,12 +3,12 @@ from inv import INV
 
 mpu = mpu6050(0x68)
 
-acc_x_multiplier = 0.98911376953125
-acc_y_multiplier = 1.00009765625
-acc_z_multiplier = 1.01008544921875
-acc_x_offset = 0.0135498046875
-acc_y_offset = -0.019622395833333334
-acc_z_offset = -0.09076985677083334
+acc_x_multiplier = 0.9948828125
+acc_y_multiplier = 0.99984619140625
+acc_z_multiplier = 1.01486572265625
+acc_x_offset = 0.013611653645833333
+acc_y_offset = -0.01932861328125
+acc_z_offset = -0.076376953125
 
 gyr_x_offset = 1.4427480916030535
 gyr_y_offset = -0.7709923664122138
@@ -38,7 +38,7 @@ class MPU:
         inv = INV(
         acc_x = (accRaw['x'] * acc_x_multiplier) + acc_x_offset,
         acc_y = (accRaw['y'] * acc_y_multiplier) + acc_y_offset,
-        acc_z = (accRaw['z'] * acc_z_multiplier) + acc_z_offset,
+        acc_z = (accRaw['z'] * acc_z_multiplier) + acc_z_offset + 0.02,
         #Gyroscope data is not callibrated as it's more precise and doesn't drift much
         gyr_x = gyrRaw['x'] + gyr_x_offset,
         gyr_y = gyrRaw['y'] + gyr_y_offset,
